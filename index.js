ac58f45cde2edfea8c73ae3e5fed2aa828656806
Mohamed Atef <atefhamosa@gmail.com>
2023-01-30 17:15:44 -0800

    U

--- index.js
+++ index.js
@@ -5,20 +5,26 @@ const urlParams = new URLSearchParams(queryString);
 const success = urlParams.get("success");
 
 const h1 = document.getElementsByTagName("h1")[0];
+const pr = document.getElementsByTagName("pr")[0];
 
 let message = "";
 
 if (success != null) {
   if (success) {
     message = "Congratulations!";
+    sub =
+      "Your payment is success, now you can go to the app and have more fun!";
   } else {
     message = "Sorry, payment has failed!";
+    sub = "Your payment has failed, please make sure you enter valid data.";
   }
 } else {
   message = "Sorry, no data available!";
+  sub = "";
 }
 
 h1.textContent = message;
+pr.textContent = sub;
 
 const btn = document.getElementsByTagName("button")[0];
 
